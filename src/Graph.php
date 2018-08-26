8c5fcb670b1d93662791c589ceaebe880fbc191a
Timothy Seebus <timothyseebus@tools4schools.org>
2018-08-26 14:14:27 +0100

    move and update ResourceIn and Resource class
    Move resourceIn and resource functions to the new Graph Class and update with cleaner code from Nova

--- src/Graph.php
+++ src/Graph.php
@@ -32,3 +32,43 @@ class Graph
     }
 
 
+    /**
+     * Register the given reources
+     *
+     * @param array $resources
+     * @return Graph
+     */
+    public static function resources(array $resources)
+    {
+        static::$resources = array_merge(static::$resources,$resources);
+
+        return new static;
+    }
+
+    /**
+     * Register all of the resource classes in the given directory.
+     *
+     * @param $directory
+     * @return void
+     */
+    public static function resourceIn($directory)
+    {
+        $namespace = app()->getNamespace();
+
+        $resources = [];
+
+        foreach ((New Finder)->in($directory)->files() as $resource){
+            $resource = $namespace.str_replace(
+                ['/','.php'],
+                ['\\',''],
+                Str::after($resource->getPathname(),app_path().DIRECTORY_SEPARATOR)
+                );
+            if(is_subclass_of($resource,Resource::class) && !(new ReflectionClass($resource))->isAbstract())
+            {
+                $resources[] = $resource;
+            }
+        }
+
+        static::resources(collect($resources)->sort()->all());
+    }
+}
